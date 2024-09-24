c41f0be3eb878ff34a96c561142c9438a6b8c040
fedemelo <f.melo@uniandes.edu.co>
2024-09-24 10:49:00 -0500

    Create new uniandes.edu.UniTrade Swift project

---
+++ UniTrade/UniTradeUITests/UniTradeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  UniTradeUITestsLaunchTests.swift
+//  UniTradeUITests
+//
+//  Created by Federico Melo Barrero on 24/09/24.
+//
+
+import XCTest
+
+final class UniTradeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
