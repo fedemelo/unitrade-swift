93cd7bda249c4e96abf75ef8225971d1dcad0217
SantiagoM99 <s.martinezn@uniandes.edu.co>
2024-10-09 19:53:48 -0500

    Implement Search Bar and Product view

--- UniTrade/UniTrade/SearchandFilterBar.swift
+++ UniTrade/UniTrade/SearchandFilterBar.swift
@@ -8,27 +8,28 @@
 import SwiftUI
 
 struct SearchandFilterBar: View {
-    
     @Binding var isFilterPresented: Bool
+    @Binding var searchQuery: String  // Binding to hold the search query
     
     var body: some View {
         HStack {
-            // Magnifying Glass
+            // Magnifying Glass Icon
             Image(systemName: "magnifyingglass")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 20, height: 20)
                 .foregroundStyle(Color.DesignSystem.primary900())
-            Spacer()
-            // Text
-            VStack(alignment: .leading, spacing: 2){
-                Text("What are you looking for ?")
-                    .font(Font.DesignSystem.bodyText300)
-                    .foregroundStyle(Color.DesignSystem.primary600())
-                
-            }
             
             Spacer()
+            // Search TextField
+            TextField("What are you looking for?", text: $searchQuery)
+                .font(Font.DesignSystem.bodyText300)
+                .foregroundStyle(Color.DesignSystem.primary600())
+                .textFieldStyle(PlainTextFieldStyle())
+                .padding(.leading, 10)
+            
+            Spacer()
+            
             // Filter Button
             Button(action: {
                 isFilterPresented.toggle()
@@ -36,18 +37,19 @@ struct SearchandFilterBar: View {
                 Image(systemName: "slider.horizontal.3")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 25, height: 25) // Adjust size as needed
-                    .foregroundStyle(Color.DesignSystem.primary900()) // Set your custom color
+                    .frame(width: 25, height: 25)
+                    .foregroundStyle(Color.DesignSystem.primary900())
             })
         }
-        .padding(.horizontal)
-        .padding(.vertical,15)
-        .overlay{
+        .padding(.horizontal, 20)
+        .padding(.vertical, 15)
+        .overlay {
             RoundedRectangle(cornerRadius: 15)
                 .stroke(lineWidth: 0.8)
                 .foregroundStyle(Color.DesignSystem.primary900())
         }
-        .padding()
+        .padding(.vertical)
+        .padding(.horizontal, 30)
     }
 }
 
